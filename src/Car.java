3a5767cf22cd7369a26f12febe5d60d65fcbfde9
User <333@fd.com>
2022-11-14 15:21:49 +0400

    HW

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,22 @@
 public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    Car(String brand, String model, double engineVolume, String color, String country, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.country = country;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + ", сборка — " + country + ", цвет кузова — " + color + ", объем двигателя — " + engineVolume + " л, год выпуска — " + year + " год.";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Car ladaGranta = new Car("Lada", "Granta", 1.7, "желтый", "Россия",2015);
+        Car audiA8 = new Car("Audi", "A8", 3.0, "черный", "Германия",2020);
+        Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", "Германия",2021);
+        Car kiaSportage = new Car("KIA", "Sportage 4-го поколения", 2.4, "красный", "Южная Корея",2018);
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6, "оранжевый", "Южная Корея",2016);
+        System.out.println(ladaGranta.toString());
+        System.out.println(audiA8.toString());
+        System.out.println(bmwZ8.toString());
+        System.out.println(kiaSportage.toString());
+        System.out.println(hyundaiAvante.toString());
 
     }
+
 }
